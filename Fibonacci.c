67655f089d4706d4d54b50c56aa4f407386238f6
Mithlesh Kumar <mithlesh4257@iitg.ernet.in>
2018-02-02 22:51:46 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,5 +1,5 @@
 ------------------------------------------
-      New Method more optimized
+      Method 1
 ------------------------------------------
 #include<stdio.h>
 
@@ -50,6 +50,35 @@ int main(){
 ------------------------------------------
       New Method more optimized
 ------------------------------------------
+
+The formula can be derived from above matrix equation.
+fibonaccimatrix
+
+Taking determinant on both sides, we get
+(-1)n = Fn+1 Fn-1 – Fn2
+Moreover, since AnAm = An+m for any square matrix A, the following 
+identities can be derived (they are obtained form two different 
+coefficients of the matrix product)
+
+Fm Fn + Fm-1 Fn-1 = Fm+n-1
+
+By putting n = n+1,
+
+Fm Fn+1 + Fm-1 Fn = Fm+n
+
+Putting m = n
+
+F2n-1 = Fn2 + Fn-12
+
+F2n = (Fn-1 + Fn+1)Fn = (2Fn-1 + Fn)Fn (Source: Wiki)
+
+To get the formula to be proved, we simply need to do following
+If n is even, we can put k = n/2
+If n is odd, we can put k = (n+1)/2
+
+Below is the implementation of above idea.
+
+-----------------------------------------
 
 // C++ Program to find n'th fibonacci Number in
 // with O(Log n) arithmatic operations
